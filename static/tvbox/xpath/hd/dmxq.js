b2e9f94633ea2308b87e65919eb39e2540690f61
wanjune <wanjune@icloud.com>
2024-12-28 00:19:47 +0800

    2412280029

--- static/tvbox/xpath/hd/dmxq.js
+++ static/tvbox/xpath/hd/dmxq.js
@@ -4,7 +4,7 @@ var rule = {
   title:'大米星球',
   desc:'https://www.dami5tv.com https://www.damiq.cc https://dmdyy.vip https://dmxq.net https://www.dmdy.vip https://www.dmdyy.cc/',
   模板:'mxpro',
-  host:'https://dmcin.club',
+  host:'https://www.88nfzm47k.wiki',
   url:'/vodshow/fyclassfyfilter.html',
   searchable:2,
   quickSearch:1,
